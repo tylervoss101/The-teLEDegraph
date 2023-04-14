e88f6649ee6808d4e9f529ed0f61b04cd4c0e495
Tyler Voss <99094850+tylervoss101@users.noreply.github.com>
2023-04-14 14:49:11 -0400

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,29 @@
+
+# Dictionary representing the morse code chart
+#Got help from geeksforgeeks.org/morse-code/translator-python
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-', '!': '-.-.--'}
+
+
+def encrypt(message):
+    message = message.upper()
+    cipher = ''
+    for char in message:
+        if char != ' ':
+            cipher += MORSE_CODE_DICT[char] + ' '
+        else:
+            cipher += ' '
+    return cipher
